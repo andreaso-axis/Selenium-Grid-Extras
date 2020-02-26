735c0fce929e5c3551298742da6e47d083765b9e
andreaso-axis <andreas.olsson@axis.com>
2020-02-26 14:29:03 +0100

    Updated Microsoft Web Driver path

--- SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/config/driver/EdgeDriver.java
+++ SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/config/driver/EdgeDriver.java
@@ -4,7 +4,15 @@ package com.groupon.seleniumgridextras.config.driver;
 public class EdgeDriver  extends DriverInfo {
     @Override
     public String getExecutablePath() {
-        return "C:\\Program Files (x86)\\Microsoft Web Driver\\" + getExecutableName();
+      private new_path = "C:\\windows\\syswow64\\" + getExecutableName(); 
+      private old_path = "C:\\Program Files (x86)\\Microsoft Web Driver\\" + getExecutableName();
+
+      File testFile = new File(new_path);
+      boolean exists = testFile.exists();
+      if(exists) {
+	return new_path;
+      }
+        return old_path;
     }
 
     @Override
